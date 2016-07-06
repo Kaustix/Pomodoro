636ee4474f59b048bad76e484dd49ac4ce5712b8
Kaustix <Kiernan.Lamarche@gmail.com>
2016-07-05 23:43:48 -0600

    Added reset button and styled the timer

---
+++ js/Extensions/DateTimeExtension.js
@@ -0,0 +1,7 @@
+export function secondsToString(seconds) {
+	var d = Number(seconds)
+	var h = Math.floor(d / 3600);
+	var m = Math.floor(d % 3600 / 60);
+	var s = Math.floor(d % 3600 % 60);
+	return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+}
--- js/Timer.jsx
+++ js/Timer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as dateTimeExtension from './Extensions/DateTimeExtension'
 
 export default class extends React.Component {
 
@@ -12,12 +13,14 @@ export default class extends React.Component {
 		this.toggleTimer = this.toggleTimer.bind(this);
 		this.tick = this.tick.bind(this);
 		this.timerDone = this.timerDone.bind(this);
+		this.resetTimer = this.resetTimer.bind(this);
 	}
 
 	tick() {
-		this.setState({ secondsRemaining: this.state.secondsRemaining - 1});
+		this.setState({secondsRemaining: this.state.secondsRemaining - 1});
 		if (this.state.secondsRemaining === 0) {
 			this.timerDone();
+			this.resetTimer();
 		}
 	}
 
@@ -29,22 +32,34 @@ export default class extends React.Component {
 		if (!this.state.isRunning) {
 			this.timer = setInterval(this.tick, 1000);
 		} else {
- 			this.timerDone();
+			this.timerDone();
 		}
 
 		this.setState({isRunning: !this.state.isRunning});
 	}
 
+	resetTimer() {
+		this.setState({isRunning: false, secondsRemaining: 5});
+	}
+
 	render() {
 		const buttonClass = this.state.isRunning ? 'btn btn-danger' : 'btn btn-success';
-		const buttonText =  this.state.isRunning ? 'Stop' : 'Start';
+		const buttonText = this.state.isRunning ? 'Stop' : 'Start';
 		return (
 			<div className="container">
 				<div className="jumbotron text-center">
-					<h1>{this.state.secondsRemaining}</h1>
+					<h1>{dateTimeExtension.secondsToString(this.state.secondsRemaining)}</h1>
 				</div>
-				<div>
-					<button id="time" type="button" className={buttonClass} onClick={this.toggleTimer}>{buttonText} Pomodoro!</button>
+				<div className="row">
+						<div className="col-sm-3 col-sm-offset-3">
+							<button type="button" className={`${buttonClass} btn-block`} onClick={this.toggleTimer}>
+								{buttonText} Pomodoro!
+							</button>
+						</div>
+						<div className="col-sm-3">
+							<button type="button" className="btn btn-info btn-block" onClick={this.resetTimer}>Rest</button>
+						</div>
+
 				</div>
 			</div>
 		)
